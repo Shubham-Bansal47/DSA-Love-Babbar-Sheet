2a2a72fccf005042b658f3b72ba5947d8c75cefc
shubham <shubhambansal096@gmail.com>
2022-01-19 01:01:13 +0530

    add

---
+++ Array/delete-file-command.py
@@ -0,0 +1,9 @@
+import os
+
+dir_path = r'F:\DSA\LoveBabbarSheet'
+file_extenstion = '.exe' # You can change it based on your need. 
+
+for root, _, files in os.walk(dir_path):
+    for file in files: 
+        if file.endswith(file_extenstion): # for each file in the dir and the sub directories, if the file name ends with the '.exe'
+            os.remove(os.path.join(root, file)) # Just delete it
