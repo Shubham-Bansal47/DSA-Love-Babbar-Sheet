847b5c59a1ecd4b80dc0c489547118d9efb4f303
shubham <shubhambansal096@gmail.com>
2022-01-21 18:50:27 +0530

    4th Day

--- Array/count-pairs-with-given-sum.cpp
+++ Array/count-pairs-with-given-sum.cpp
@@ -8,13 +8,21 @@ using namespace std;
 int main()
 {
     FastIO;
-    int i,j,n;
-    cin>>n;
+    int i,j,n,k,sum=0;
+    cin>>n>>k;
     int arr[n];
     for(i=0; i<n; i++)
     {
         cin>>arr[i];
     }
-    sort(arr,arr+n);
-    
+    unordered_map<int,int> m;
+    for(i=0; i<n; i++)
+    {
+        if(m[k-arr[i]]>0)
+        {
+            sum+=m[k-arr[i]];
+        }
+        m[arr[i]]++;
+    }
+    cout<<sum;
 }
